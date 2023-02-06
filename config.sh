415ce1bf75de6b5d415f93e6bc63a0d3073b7882
Sean Cornillie <34557093+spcornil@users.noreply.github.com>
2023-02-06 09:38:14 -0700

    update config

--- config.sh
+++ config.sh
@@ -1,5 +1,4 @@
 ### Basic ssh config constructed using https://linux.die.net/man/5/ssh_config
-
 Host aws-ec2-micro
     HostName ec2-<host-name>.us-west-2.compute.amazonaws.com
     User <username>
