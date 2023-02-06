f8713e6e1b868c33d78a0731532155371205f96f
Sean Cornillie <34557093+spcornil@users.noreply.github.com>
2023-02-06 09:23:45 -0700

    created config

---
+++ config.sh
@@ -0,0 +1,6 @@
+### Basic ssh config constructed using https://linux.die.net/man/5/ssh_config
+
+Host aws-ec2-micro
+    HostName ec2-<host-name>.us-west-2.compute.amazonaws.com
+    User <username>
+    IdentityFile <path-to-key>/<key-name>.pem
